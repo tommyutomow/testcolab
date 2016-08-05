ea4e00ef0230d2b33080c1661b504dafeff4b449
Reksandhy <reksandhykadari@yahoo.co.id>
2016-08-05 11:20:55 +0700

    $a perubahan final

--- index.php
+++ index.php
@@ -9,15 +9,8 @@
 	<p>aku adalah anak gembala!</p><p>aku adalah anak gembala!</p><p>aku adalah anak gembala!</p>
 
 		<?php
-<<<<<<< HEAD
-<<<<<<< HEAD
 			$a = 5+5;
-=======
-			$a = 10+12;
->>>>>>> origin/master
-=======
-			$a = 1+3+9+10;
->>>>>>> origin/master
+
 			$b = 20;
 			$c =30;
 			$d= 1;
