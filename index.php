7bb81ca1b39d840be3ee0a943b482e9e38acb953
ReynaldiTan <reynaldi.chen95@gmail.com>
2016-08-05 11:06:06 +0700

    banyak anak gembala

--- index.php
+++ index.php
@@ -6,12 +6,13 @@
 	<h1>Result:</h1>
 	<h2>ini title biasa</h2>
 	<p>aku adalah anak gembala!</p>
+	<p>aku adalah anak gembala!</p><p>aku adalah anak gembala!</p><p>aku adalah anak gembala!</p>
+
 		<?php
 			$a = 1;
 			$b = 20;
 			$c =30;
 			$d= 1;
-			$e= 3;
 			$b = $a+$c+$e;
 			$a=$a+1+$b+$d+$e;
 			echo $a;
